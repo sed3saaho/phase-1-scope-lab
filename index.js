6a3dec6984cd79cacd142fef6e7a52e19f893028
Sedly <sedly.keith@student.moringaschool.com>
2024-04-11 10:47:58 +0300

    phase-1-scope-lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+var bestCustomer;
+
+function upperCaseCustomerName() {
+    return customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'; // Note: This is not recommended in production code
+}
+
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+
+const leastFavoriteCustomer = 'someone';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'maybe bob';
+    leastFavoriteCustomer = 'someone else';
+}
+console.log(upperCaseCustomerName(customerName));
